cd52e8bc02d0cd143bfff20663a186e73fca96bb
Rein F <public@reinfernhout.xyz>
2020-02-09 17:56:59 +0100

    done creating instruction stack

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define INST_STACK_LENGTH 512
+#define STACK_SIZE 30000
+
 int main(int argc, char *argv[]) {
 	if (argc < 2) {
 		puts("Please specify a filename!");
@@ -18,23 +21,20 @@ int main(int argc, char *argv[]) {
 		return EXIT_FAILURE;
 	}
 
-	char inst[512];
+	//Instruction stack is 512 bytes
+	char inst[INST_STACK_LENGTH] = {0};
+
+	//There are 8 instructions supported
 	char *av_insts[8] = {">", "<", "+", "-", ".", ",", "[", "]"};
 
 	//Write the stack
-	int c, i, j;
-	while ((c = fgetc(fs))/*  != EOF */) {
+	char c;
+	int i = 0, j;
+	while ((c = fgetc(fs))  != EOF ) {
 
-		if (c == -1) {
-			printf("Last 6: %c%c%c%c%c%c\n", inst[i-6], inst[i-5], inst[i-4], inst[i-3], inst[i-2], inst[i-1]);
-			break;
-		}
+		//printf(&c);
 
-/* 		if (c < 0) {
-			break;
-		} */
-
-		if (i > 511) {
+		if (i > INST_STACK_LENGTH -1) {
 			perror("Maximum instruction stack reached.");
 			return EXIT_FAILURE;
 		} 
@@ -42,16 +42,27 @@ int main(int argc, char *argv[]) {
 		//Add to instructions if valid inst
 		for (j=0; j < 8; j++) {
 			if (*av_insts[j] == c) {
+				//printf("%c %d\n", c, i);
 				inst[i] = c;
 				i++;
-				break;
 			}
 		}
 		
 	}
 
-	for (i=0; i < 512; i++)
-		printf("%s", &inst[i]);
+	puts("Instruction stack:");
+	printf(inst);
+
+
+
+	//Setup WM
+	char stack[STACK_SIZE] = {0}; //Initilize stack
+	char *sp = stack; //Point sp to stack
+
+	//Run program
+	for (i = 0, i == NULL, i++) {
+
+	}
 
 	fclose(fs);
 
